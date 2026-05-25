84d1da9e4e31eefb7d9107dd7d983128adc89ecd
urvilranpariya <urvilranpariya61@gmail.com>
2026-05-25 09:35:17 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="Ranpariya Urvil",
+    author_email='urvilranpariya61@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)   
